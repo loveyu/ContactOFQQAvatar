900473cecc0326ed777e4719eb817ba100c61849
loveyu <admin@loveyu.info>
2014-09-13 18:32:52 +0800

    代码优化

--- app/src/main/java/net/loveyu/contactofqqavatar/Report.java
+++ app/src/main/java/net/loveyu/contactofqqavatar/Report.java
@@ -1,13 +1,9 @@
 package net.loveyu.contactofqqavatar;
 
-import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Context;
-import android.content.DialogInterface;
-import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
-import android.net.Uri;
 import android.os.Build;
 import android.telephony.TelephonyManager;
 import android.text.Html;
@@ -16,11 +12,8 @@ import android.text.util.Linkify;
 import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
-
 import net.loveyu.contactofqqavatar.listener.UpdateClick;
-
 import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
