50849e9be74064888b8f6ed6f7bee5aa19addd37
loveyu <i@loveyu.info>
2017-04-10 02:11:40 +0800

    修正权限无法正常判断的问题

--- app/src/main/java/net/loveyu/contactofqqavatar/LaunchActivity.java
+++ app/src/main/java/net/loveyu/contactofqqavatar/LaunchActivity.java
@@ -9,26 +9,39 @@ import android.content.Intent;
 public class LaunchActivity extends Activity {
     Intent intent;
 
+    private boolean startCheck = true;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lanuch);
         intent = new Intent(this, MainActivity.class);
 
-        if(Permissions.getInstance(this).check()){
-            finish();
-            return;
-        }
+        startCheck = Permissions.getInstance(this).check();
 
         new Thread(new Runnable() {
 
             @Override
             public void run() {
-                if (Contact.selfList == null || Contact.selfList.size() < 1) {
-                    Contact.selfList = ContactAction.getContact(getContentResolver());
-                    Contact.selectList = new ArrayList<Integer>();
-                } else {
-                    MainActivity.NotifyDataRefresh();
+                if (!LaunchActivity.this.startCheck) {
+                    while (Permissions.check_status == 0) {
+                        try {
+                            Thread.sleep(200);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    if (Permissions.check_status == 1) {
+                        LaunchActivity.this.startCheck = true;
+                    }
+                }
+                if(LaunchActivity.this.startCheck) {
+                    if (Contact.selfList == null || Contact.selfList.size() < 1) {
+                        Contact.selfList = ContactAction.getContact(getContentResolver());
+                        Contact.selectList = new ArrayList<Integer>();
+                    } else {
+                        MainActivity.NotifyDataRefresh();
+                    }
                 }
                 goMain();
             }
@@ -40,7 +53,11 @@ public class LaunchActivity extends Activity {
     }
 
     private void goMain() {
-        startActivity(intent);
+        if (startCheck) {
+            startActivity(intent);
+        }else{
+
+        }
         finish();
     }
 }
--- app/src/main/java/net/loveyu/contactofqqavatar/Permissions.java
+++ app/src/main/java/net/loveyu/contactofqqavatar/Permissions.java
@@ -6,17 +6,21 @@ import android.content.Context;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.os.Build;
+import android.os.SystemClock;
 import android.util.Log;
 
 public class Permissions {
 
     public static final int MY_PERMISSIONS_REQUEST_READ_CONTACTS = 1000;
 
+    public static int check_status = 0;
+
     private Activity context;
 
     private static Permissions instance = null;
 
     public static Permissions getInstance(Activity context) {
+        check_status = 0;
         if (instance == null) {
             instance = new Permissions(context);
         }
@@ -28,15 +32,17 @@ public class Permissions {
     }
 
     public boolean check() {
+        check_status = 0;
         return PermissionGranted();
     }
 
 
     public boolean PermissionGranted() {
-        boolean result = context.checkSelfPermission(Manifest.permission.READ_CONTACTS)
-                == PackageManager.PERMISSION_GRANTED &&
-                context.checkSelfPermission(Manifest.permission.WRITE_CONTACTS)
-                        == PackageManager.PERMISSION_GRANTED;
+        boolean result =
+                context.checkSelfPermission(Manifest.permission.READ_CONTACTS)
+                        == PackageManager.PERMISSION_GRANTED &&
+                        context.checkSelfPermission(Manifest.permission.WRITE_CONTACTS)
+                                == PackageManager.PERMISSION_GRANTED;
         if (!result) {
             context.requestPermissions(new String[]{
                             Manifest.permission.READ_CONTACTS,
@@ -45,19 +51,19 @@ public class Permissions {
                     MY_PERMISSIONS_REQUEST_READ_CONTACTS);
         }
 
-
         return result;
     }
 
     public static void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        check_status = 3;
         switch (requestCode) {
             case MY_PERMISSIONS_REQUEST_READ_CONTACTS: {
-                // If request is cancelled, the result arrays are empty.
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-//                    is_deny = false;
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Log.d("CHECK:", "B3");
+                    check_status = 1;
                 } else {
-//                    is_deny = true;
+                    Log.d("CHECK:", "B4");
+                    check_status = 2;
                 }
             }
         }
